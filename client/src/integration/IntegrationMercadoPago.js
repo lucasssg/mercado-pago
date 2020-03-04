4df976c362b55bb94f8b26185e4f3b95208a9c2b
Lucas˜git config --global user.name Lucasgit config --global user.name Lucas˜ <lucas.goncalves@clinicorp.com>
2020-03-04 13:40:39 -0300

    Added CSS

--- client/src/integration/IntegrationMercadoPago.js
+++ client/src/integration/IntegrationMercadoPago.js
@@ -124,167 +124,125 @@ class Integration extends React.Component {
   render() {
     return (
       <div style={{}}>
-        <h1 style={{textAlign:"center",marginTop:20}}>Insira os dados do Cartão de Crédito</h1>
-        <form action="http://localhost:3000/process_payment" method="post" id="pay" name="pay" >
-        <TextField 
-          id="cardholderName"
-          label="Nome do Cartão"
-          lineDirection="center"
-          placeholder="Nome do Cartão"
-          className="md-cell md-cell--bottom"
-          value="MARCIO H. MEIER"
-        />
-        <TextField 
-          id="cardNumber"
-          label="Número do Cartão"
-          lineDirection="center"
-          placeholder="Número do Cartão"
-          className="md-cell md-cell--bottom"
-          value="4235647728025682"
-        />
-        <TextField 
-          id="cardExpirationMonth"
-          label="Mês Vencimento"
-          lineDirection="center"
-          placeholder="Mês Vencimento"
-          className="md-cell md-cell--bottom"
-          value="10"
-        />
-        <TextField 
-          id="cardExpirationYear"
-          label="Ano Vencimento"
-          lineDirection="center"
-          placeholder="Ano Vencimento"
-          className="md-cell md-cell--bottom"
-          value="2023"
-        />
-        <TextField 
-          id="securityCode"
-          label="Código Segurança"
-          lineDirection="center"
-          placeholder="Código Segurança"
-          className="md-cell md-cell--bottom"
-          value="999"
-        />
-         <TextField 
-          id="paymentMethodId"
-          label="Bandeira"
-          lineDirection="center"
-          placeholder="MasterCard, Visa..."
-          className="md-cell md-cell--bottom"
-          value="visa"
-        />
-        <TextField 
-          id="docType"
-          label="Tipo Documento"
-          lineDirection="center"
-          placeholder="CPF"
-          className="md-cell md-cell--bottom"
-          value="CPF"
-        />
-        <TextField 
-          id="docNumber"
-          label="Número Documento"
-          lineDirection="center"
-          placeholder="Digite seu CPF ou RG"
-          className="md-cell md-cell--bottom"
-          value="09775195977"
-        />
-         <TextField 
-          id="description"
-          label="Descrição da Compra"
-          lineDirection="center"
-          placeholder="Descrição"
-          className="md-cell md-cell--bottom"
-          value="Julinho fdp"
-        />
-        <TextField 
-          id="amount"
-          name="amount"
-          label="Valor da Compra"
-          lineDirection="center"
-          placeholder="Valor"
-          className="md-cell md-cell--bottom"
-          value="129"
-        />
-        <TextField 
-          id="installments"
-          name="installments"
-          label="Qtde. Parcelas"
-          lineDirection="center"
-          placeholder="Parcelas"
-          className="md-cell md-cell--bottom"
-          value="2"
-        />
-         <TextField 
-          id="email"
-          name="email"
-          label="Email"
-          lineDirection="center"
-          placeholder="Email"
-          className="md-cell md-cell--bottom"
-          value="lucas@gmail.com"
-        />
-        <input type="submit" value="ENVIAR" />
+        <h2 style={{textAlign:"center",marginTop:20}}>DADOS DO CARTÃO DE CRÉDITO</h2>
+        <form style={{padding:10}} action="http://localhost:3000/process_payment" method="post" id="pay" name="pay" >
+          <div style={{display:"flex"}}>
+            <TextField 
+              id="cardholderName"
+              label="Nome do Cartão"
+              lineDirection="center"
+              placeholder="Nome do Cartão"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="MARCIO H. MEIER"
+            />
+            <TextField 
+              id="cardNumber"
+              label="Número do Cartão"
+              lineDirection="center"
+              placeholder="Número do Cartão"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="4235647728025682"
+            />
+          </div>
+          <div style={{display:"flex"}}>
+            <TextField 
+              id="cardExpirationMonth"
+              label="Mês Vencimento"
+              lineDirection="center"
+              placeholder="Mês Vencimento"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="10"
+            />
+            <TextField 
+              id="cardExpirationYear"
+              label="Ano Vencimento"
+              lineDirection="center"
+              placeholder="Ano Vencimento"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="2023"
+            />
+          </div>
+          <div style={{display:"flex"}}>
+            <TextField 
+              id="securityCode"
+              label="Código Segurança"
+              lineDirection="center"
+              placeholder="Código Segurança"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="999"
+            />
+            <TextField 
+              id="paymentMethodId"
+              name="paymentMethodId"
+              label="Bandeira"
+              lineDirection="center"
+              placeholder="MasterCard, Visa..."
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="visa"
+            />
+          </div>
+          <div style={{display:"flex"}}>
+            <TextField 
+                id="docType"
+                label="Tipo Documento"
+                lineDirection="center"
+                placeholder="CPF"
+                className="md-cell md-cell--6 md-cell--bottom"
+                value="CPF"
+            />
+            <TextField 
+              id="docNumber"
+              label="Número Documento"
+              lineDirection="center"
+              placeholder="Digite seu CPF ou RG"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="09775195977"
+            />
+          </div>
+          <div style={{display:"flex"}}>
+            <TextField 
+              id="description"
+              name="description"
+              label="Descrição da Compra"
+              lineDirection="center"
+              placeholder="Descrição"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="Jogo de Calota Renault Clio Aro 15"
+            />
+            <TextField 
+              id="amount"
+              name="amount"
+              label="Valor da Compra"
+              lineDirection="center"
+              placeholder="Valor"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="129"
+            />
+          </div>
+          <div style={{display:"flex"}}>
+            <TextField 
+              id="installments"
+              name="installments"
+              label="Qtde. Parcelas"
+              lineDirection="center"
+              placeholder="Parcelas"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="2"
+            />
+            <TextField 
+              id="email"
+              name="email"
+              label="Email"
+              lineDirection="center"
+              placeholder="Email"
+              className="md-cell md-cell--6 md-cell--bottom"
+              value="lucas@gmail.com"
+            />
+          </div>
+          <div style={{marginTop:"5%", display:'flex', justifyContent:"center"}}>
+            <input style={{cursor:"pointer", padding:5, fontSize:20, backgroundColor:"green", borderRadius:10, color:"white"}} type="submit" value="CONFIRMAR COMPRA" />
+          </div>
         </form>
-        {/* <form action="http://localhost:3000/process_payment" method="post" id="pay" name="pay" >
-          <fieldset>
-            <ul>
-              <li>
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" value="lucas_matheus@estudante.sc.senai.br" />
-              </li>
-              <li>
-                <label for="cardNumber">Credit card number:</label>
-                <input type="text" id="cardNumber" data-checkout="cardNumber" value="4235647728025682" onselectstart="return false" />
-              </li>
-              <li>
-                <label for="securityCode">Security code:</label>
-                <input type="text" id="securityCode" data-checkout="securityCode" value="999" onselectstart="return false" />
-              </li>
-              <li>
-                <label for="cardExpirationMonth">Expiration month:</label>
-                <input type="text" id="cardExpirationMonth" data-checkout="cardExpirationMonth" value="10" />
-              </li>
-              <li>
-                <label for="cardExpirationYear">Expiration year:</label>
-                <input type="text" id="cardExpirationYear" data-checkout="cardExpirationYear" value="2023" />
-              </li>
-              <li>
-                <label for="cardholderName">Card holder name:</label>
-                <input type="text" id="cardholderName" data-checkout="cardholderName" value="MARCIO H. MEIER" />
-              </li>
-              <li>
-                <label for="docType">Document type:</label>
-                <input id="docType" data-checkout="docType" value="CPF" readonly></input>
-              </li>
-              <li>
-                <label for="docNumber">Document number:</label>
-                <input type="text" id="docNumber" data-checkout="docNumber" value="09775195977" />
-              </li>
-              <li>
-                <label for="installments">Installments:</label>
-                <input type="number" id="installments" class="form-control" name="installments" value="2" readonly></input>
-              </li>
-              <li>
-                <label for="amount">Amount:</label>
-                <input id="amount" class="form-control" name="amount" value="129"></input>
-              </li>
-              <li>
-                <label for="description">Descrição:</label>
-                <input id="description" class="form-control" name="description" value="FILHA DA PUTA"></input>
-              </li>
-              <li>
-                <label for="paymentMethodId">Bandeira:</label>
-                <input id="paymentMethodId" class="form-control" name="paymentMethodId" value="visa"></input>
-              </li>
-            </ul>
-            <input type="hidden" name="amount" id="amount" />
-            <input type="hidden" name="description" />
-            <input type="hidden" name="paymentMethodId" />
-            <input type="submit" value="Pay!" />
-          </fieldset>
-        </form> */}
       </div>
     );
   }
